0333102ce2198288d5e421416692f0f659ef81df
MacYang96 <47791928+MacYang96@users.noreply.github.com>
2021-02-14 23:45:49 +0100

    removed Bot/Top mr

--- MemoryRegion.fs
+++ MemoryRegion.fs
@@ -1,14 +1,9 @@
-(* Represents a memory region: can be an AR, Heap or Global region 
- * There are two other constructor, not really memory region.
- * Top and Bottom are special value to create the Top/Bot VS element
- *)
+(* Represents a memory region: can be an AR, Heap or Global region  *)
 
 type RegionType =
 | AR = 0
 | Global = 1
 | Heap = 2
-| Top = 3
-| Bottom = 4
 ;;
 
 type MemoryRegion(regType : RegionType, id : int) =
