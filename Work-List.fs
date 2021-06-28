5153701e4af33c849c9d6562ed5e2335e1577fb2
MacYang96 <47791928+MacYang96@users.noreply.github.com>
2021-06-28 19:39:11 +0200

    Optimized inefficiency caused by list using a resizeArray

--- Work-List.fs
+++ Work-List.fs
@@ -14,15 +14,15 @@ let WorkList (L : ILattice<'T>) (T : 'E -> ('T->'T)) (cfg : ICFG<'E>)  =
     // Note: //Dict is mutable, as opposed to Map, and is generic, as opposed to htable
     let n = cfg.Length()
     let x = new Dictionary<INode<'E>,'T>()
-    let mutable W = []
+    let W = new ResizeArray<INode<'E>>()
     for node in cfg.Nodes() do
         x.TryAdd(node, L.Bot()) |> ignore // ignore existing nodes (case of several branches that converges)
-        W <- W@[node]
+        W.Add(node)
 
     // Scans work-list and applies transfer function based on type of statement
-    while W.Length <> 0 do
-        let vi = W.Head
-        W <- W.Tail
+    while W.Count <> 0 do
+        let vi = W.[0]
+        W.RemoveAt(0)
         let y = x.[vi] |> T(vi.Statm())
 
         // for each vi's successor node, compute join and, if It's not already present, add It
@@ -31,6 +31,6 @@ let WorkList (L : ILattice<'T>) (T : 'E -> ('T->'T)) (cfg : ICFG<'E>)  =
             let z = L.Widen x.[vj] y   // ensure termination
             if x.[vj] <> z then 
                 x.[vj]<-z
-                W <- W@[vj]
+                W.Add(vj)
     x
 ;;
